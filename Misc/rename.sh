6d5b2e8f37d6d050374b9eb5e9f5cfc9c6bb907e
OpenSourceImageFolder <halley7@protonmail.com>
2021-05-01 02:25:55 +0200

    Misc Folder with a variety of reaction images and other random stuff

---
+++ Misc/rename.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+n=0
+for i in *.jpg *.jpeg *.png *.gif *.JPG *.JPEG *.PNG *.GIF
+do
+    ext="$(echo "$i" | sed 's/.*\.//g')"
+    num_str="$(printf "%03d" $n)"
+    mv "$i" "misc${num_str}.${ext}"
+    echo mv "$i" "misc${num_str}.${ext}"
+    n="$(expr 1 + $n)"
+done
